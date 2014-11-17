f494d3b515c00cb89b177c430b6e6297a3c52df8
Decebal Suiu <decebal.suiu@gmail.com>
2014-11-17 18:25:30 +0200

    add copyright header to some java files

--- pippo-demo/src/main/java/ro/fortsoft/pippo/demo/spring/SpringDemo.java
+++ pippo-demo/src/main/java/ro/fortsoft/pippo/demo/spring/SpringDemo.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2014 Decebal Suiu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with
+ * the License. You may obtain a copy of the License in the LICENSE file, or at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package ro.fortsoft.pippo.demo.spring;
 
 import org.springframework.context.ApplicationContext;
@@ -13,9 +25,16 @@ public class SpringDemo {
     public static void main(String[] args) {
         Pippo pippo = new Pippo();
         pippo.getServer().getSettings().staticFilesLocation("/public");
+
+        // create spring application context
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+
+        // registering SpringControllerInjector
         pippo.getApplication().getControllerInstantiationListeners().add(new SpringControllerInjector(applicationContext));
+
+        // add controller
         pippo.getApplication().GET("/", ContactsController.class, "index");
+
         pippo.start();
     }
 
