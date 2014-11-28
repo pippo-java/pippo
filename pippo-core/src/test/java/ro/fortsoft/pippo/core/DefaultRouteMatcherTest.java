e44e644a3572cac51f2148c42056c3ac8b4c9922
James Moger <james.moger@gitblit.com>
2014-11-28 18:12:21 -0500

    Switch route parameter syntax from `:parameter` to `{parameter}`
    The `:parameter` syntax is an inflexible convention.  Using the `{parameter}` notation allows for specifying parameter regular expressions such as `{id: [0-9]+}`.

--- pippo-core/src/test/java/ro/fortsoft/pippo/core/DefaultRouteMatcherTest.java
+++ pippo-core/src/test/java/ro/fortsoft/pippo/core/DefaultRouteMatcherTest.java
@@ -15,18 +15,20 @@
  */
 package ro.fortsoft.pippo.core;
 
+import ro.fortsoft.pippo.core.route.DefaultRouteMatcher;
+import ro.fortsoft.pippo.core.route.Route;
+import ro.fortsoft.pippo.core.route.RouteMatch;
+import ro.fortsoft.pippo.core.route.WebjarsResourceHandler;
+
+import java.util.List;
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import ro.fortsoft.pippo.core.route.DefaultRouteMatcher;
-import ro.fortsoft.pippo.core.route.Route;
-import ro.fortsoft.pippo.core.route.RouteMatch;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Decebal Suiu
@@ -66,27 +68,19 @@ public class DefaultRouteMatcherTest extends Assert {
 
     @Test
     public void testInvalidMethodRequestRoute() throws Exception {
-        Route route = new Route("/*", "GETT", new EmptyRouteHandler());
+        Route route = new Route("/.*", "GETT", new EmptyRouteHandler());
         thrown.expect(Exception.class);
         thrown.expectMessage("Invalid request method");
         routeMatcher.addRoute(route);
     }
 
     @Test
-    public void testInvalidUrlPatternRoute() throws Exception {
-        Route route = new Route("$", HttpConstants.Method.GET, new EmptyRouteHandler());
-        thrown.expect(Exception.class);
-        thrown.expectMessage("Invalid url pattern");
-        routeMatcher.addRoute(route);
-    }
-
-    @Test
     public void testAddRoute() throws Exception {
-        Route route = new Route("/*", HttpConstants.Method.GET, new EmptyRouteHandler());
+        Route route = new Route("/.*", HttpConstants.Method.GET, new EmptyRouteHandler());
         routeMatcher.addRoute(route);
 
-        assertEquals(routeMatcher.getRoutes().size(), 1);
-        assertEquals(routeMatcher.getRoutes(HttpConstants.Method.GET).size(), 1);
+        assertEquals(1, routeMatcher.getRoutes().size());
+        assertEquals(1, routeMatcher.getRoutes(HttpConstants.Method.GET).size());
     }
 
     @Test
@@ -96,53 +90,100 @@ public class DefaultRouteMatcherTest extends Assert {
 
         List<RouteMatch> routeMatches = routeMatcher.findRoutes("/contact", HttpConstants.Method.GET);
         assertNotNull(routeMatches);
-        assertEquals(routeMatches.size(), 1);
+        assertEquals(1, routeMatches.size());
 
         routeMatches = routeMatcher.findRoutes("/contact", HttpConstants.Method.POST);
         assertNotNull(routeMatches);
-        assertEquals(routeMatches.size(), 0);
+        assertEquals(0, routeMatches.size());
 
         routeMatches = routeMatcher.findRoutes("/", HttpConstants.Method.GET);
         assertNotNull(routeMatches);
-        assertEquals(routeMatches.size(), 0);
+        assertEquals(0, routeMatches.size());
     }
 
     @Test
     public void testPathParamsRoute() throws Exception {
-        Route route = new Route("/contact/:id", HttpConstants.Method.GET, new EmptyRouteHandler());
+        Route route = new Route("/contact/{id}", HttpConstants.Method.GET, new EmptyRouteHandler());
         routeMatcher.addRoute(route);
 
         List<RouteMatch> routeMatches = routeMatcher.findRoutes("/contact/3", HttpConstants.Method.GET);
-        assertEquals(routeMatches.size(), 1);
+        assertEquals(1, routeMatches.size());
 
         Map<String, String> pathParameters = routeMatches.get(0).getPathParameters();
         assertNotNull(pathParameters);
-        assertEquals(pathParameters.size(), 1);
+        assertEquals(1, pathParameters.size());
         assertTrue(pathParameters.containsKey("id"));
-        assertEquals(pathParameters.get("id"), String.valueOf(3));
+        assertEquals(String.valueOf(3), pathParameters.get("id"));
     }
 
     @Test
     public void testWildcardRoute() throws Exception {
-        Route route = new Route("/*", HttpConstants.Method.GET, new EmptyRouteHandler());
+        Route route = new Route("/.*", HttpConstants.Method.GET, new EmptyRouteHandler());
         routeMatcher.addRoute(route);
 
         List<RouteMatch> routeMatches = routeMatcher.findRoutes("/contact/3", HttpConstants.Method.GET);
-        assertEquals(routeMatches.size(), 1);
+        assertEquals(1, routeMatches.size());
     }
 
     @Test
     public void testPatchRoute() throws Exception {
-        Route route = new Route("/contact/:id", HttpConstants.Method.PATCH, new EmptyRouteHandler());
+        Route route = new Route("/contact/{id}", HttpConstants.Method.PATCH, new EmptyRouteHandler());
         routeMatcher.addRoute(route);
 
         List<RouteMatch> routeMatches = routeMatcher.findRoutes("/contact/3", HttpConstants.Method.PATCH);
-        assertEquals(routeMatches.size(), 1);
+        assertEquals(1, routeMatches.size());
 
         Map<String, String> pathParameters = routeMatches.get(0).getPathParameters();
         assertNotNull(pathParameters);
-        assertEquals(pathParameters.size(), 1);
+        assertEquals(1, pathParameters.size());
         assertTrue(pathParameters.containsKey("id"));
-        assertEquals(pathParameters.get("id"), String.valueOf(3));
+        assertEquals(String.valueOf(3), pathParameters.get("id"));
+    }
+
+    @Test
+    public void testIntIdRoute() throws Exception {
+        Route route = new Route("/contact/{id: [0-9]+}", HttpConstants.Method.PATCH, new EmptyRouteHandler());
+        routeMatcher.addRoute(route);
+
+        List<RouteMatch> routeMatches = routeMatcher.findRoutes("/contact/3", HttpConstants.Method.PATCH);
+        assertEquals(1, routeMatches.size());
+
+        Map<String, String> pathParameters = routeMatches.get(0).getPathParameters();
+        assertNotNull(pathParameters);
+        assertEquals(1, pathParameters.size());
+        assertTrue(pathParameters.containsKey("id"));
+        assertEquals(String.valueOf(3), pathParameters.get("id"));
+
+        routeMatches = routeMatcher.findRoutes("/contact/a", HttpConstants.Method.PATCH);
+        assertEquals(0, routeMatches.size());
+
+    }
+
+    @Test
+    public void testIntIdRoute2() throws Exception {
+        Route route = new Route("/contact/{id: [0-9]+}/something/{else: [A-z]*}", HttpConstants.Method.GET, new EmptyRouteHandler());
+        routeMatcher.addRoute(route);
+
+        List<RouteMatch> routeMatches = routeMatcher.findRoutes("/contact/3/something/borrowed", HttpConstants.Method.GET);
+        assertEquals(1, routeMatches.size());
+
+        Map<String, String> pathParameters = routeMatches.get(0).getPathParameters();
+        assertNotNull(pathParameters);
+        assertEquals(2, pathParameters.size());
+        assertTrue(pathParameters.containsKey("id"));
+        assertEquals(String.valueOf(3), pathParameters.get("id"));
+        assertEquals("borrowed", pathParameters.get("else"));
+
+    }
+
+    @Test
+    public void testWebjarsRoute() throws Exception {
+        WebjarsResourceHandler webjars = new WebjarsResourceHandler();
+        Route route = new Route(webjars.getUrlPattern(), HttpConstants.Method.GET, new EmptyRouteHandler());
+        routeMatcher.addRoute(route);
+
+        List<RouteMatch> routeMatches = routeMatcher.findRoutes("/webjars/bootstrap/3.0.2/css/bootstrap.min.css", HttpConstants.Method.GET);
+        assertEquals(1, routeMatches.size());
+
     }
 }
