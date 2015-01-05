1f77ac0c422be3f417fc82077d1b454c0b57879a
Decebal Suiu <decebal.suiu@gmail.com>
2015-01-05 20:37:59 +0200

    add license header

--- pippo-demo/src/main/java/ro/fortsoft/pippo/demo/crudng/CrudNgApiController.java
+++ pippo-demo/src/main/java/ro/fortsoft/pippo/demo/crudng/CrudNgApiController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ro.fortsoft.pippo.demo.crudng;
 
 import ro.fortsoft.pippo.core.Param;
@@ -18,11 +33,10 @@ import org.slf4j.LoggerFactory;
  * </p>
  *
  * @author James Moger
- *
  */
 public class CrudNgApiController extends Controller {
 
-    private final Logger log = LoggerFactory.getLogger(CrudNgApiController.class);
+    private static final Logger log = LoggerFactory.getLogger(CrudNgApiController.class);
 
     ContactService getContactService() {
         return ((CrudNgApplication) getApplication()).getContactService();
