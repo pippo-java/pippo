a2052988766c4ae45652045297ac2a2dc20f3cfe
Decebal Suiu <decebal.suiu@gmail.com>
2015-03-16 02:36:05 +0200

    add missing files

---
+++ pippo-core/src/main/java/ro/pippo/core/util/CookieUtils.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ro.pippo.core.util;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Decebal Suiu
+ */
+public class CookieUtils {
+
+    public static List<Cookie> getCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if ((cookies == null) || (cookies.length == 0)) {
+            return Collections.emptyList();
+        }
+
+        return Arrays.asList(cookies);
+    }
+
+    public static Cookie getCookie(HttpServletRequest request, String name) {
+        List<Cookie> cookies = getCookies(request);
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(name)) {
+                return cookie;
+            }
+        }
+
+        return null;
+    }
+
+}
