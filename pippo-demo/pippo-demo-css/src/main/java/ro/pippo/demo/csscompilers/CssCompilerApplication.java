c8c31a585f0ef3bafe7255367132f6a384b18712
danjee <daniel.jipa@fortsoft.ro>
2015-06-23 13:15:39 +0300

    Minor fixes

--- pippo-demo/pippo-demo-css/src/main/java/ro/pippo/demo/csscompilers/CssCompilerApplication.java
+++ pippo-demo/pippo-demo-css/src/main/java/ro/pippo/demo/csscompilers/CssCompilerApplication.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ro.pippo.demo.csscompilers;
 
 import ro.pippo.core.Application;
@@ -7,7 +22,7 @@ import ro.pippo.less4j.LessResourceHandler;
 import ro.pippo.sasscompiler.SassResourceHandler;
 
 /**
- * Created by Daniel Jipa on 20.06.2015.
+ * @author Daniel Jipa
  */
 public class CssCompilerApplication extends Application {
 
--- pippo-less4j/src/main/java/ro/pippo/less4j/LessResourceHandler.java
+++ pippo-less4j/src/main/java/ro/pippo/less4j/LessResourceHandler.java
@@ -1,10 +1,23 @@
+/*
+ * Copyright (C) 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ro.pippo.less4j;
 
 import com.github.sommeri.less4j.LessCompiler;
 import com.github.sommeri.less4j.LessSource;
 import com.github.sommeri.less4j.core.ThreadUnsafeLessCompiler;
-import com.github.sommeri.less4j.utils.debugonly.DebugSysout;
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ro.pippo.core.Application;
@@ -18,7 +31,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
- * Created by Daniel Jipa on 22.06.2015.
+ * @author Daniel Jipa
  */
 public class LessResourceHandler extends StaticResourceHandler {
 
