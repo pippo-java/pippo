053685d8d1232851bdcf428c7ce24a090406f995
ScienJus <xie_enlong@foxmail.com>
2016-03-03 09:57:00 +0800

    update

--- pippo-controller/src/test/java/ControllerHandlerTest.java
+++ pippo-controller/src/test/java/ControllerHandlerTest.java
@@ -19,26 +19,33 @@ import ro.pippo.controller.DefaultControllerHandler;
 import ro.pippo.core.Param;
 import ro.pippo.core.ParamPattern;
 
+import static org.junit.Assert.assertNotNull;
+
 /**
  * @author ScienJus
- * @date 2016/3/2.
  */
 public class ControllerHandlerTest {
 
     @Test
-    public void testFindMethodWithDate() throws Exception {
-        DefaultControllerHandler handler = new DefaultControllerHandler(DateController.class, "testDate");
-        handler = new DefaultControllerHandler(DateController.class, "testDateWithPattern");
+    public void testDateParam() throws Exception {
+        DefaultControllerHandler handler = new DefaultControllerHandler(DateController.class, "testDateParam");
+        assertNotNull(handler.getMethod());
+    }
+
+    @Test
+    public void testDateParamWithPattern() throws Exception {
+        DefaultControllerHandler handler = new DefaultControllerHandler(DateController.class, "testDateParamWithPattern");
+        assertNotNull(handler.getMethod());
     }
 
     static class DateController extends Controller {
 
-        public void testDate(@Param("date") java.util.Date since) {
+        public void testDateParam(@Param("date") java.util.Date since) {
             System.out.println(since);
         }
 
         // yyyy-MM-dd HH:mm is not supported from in java.sql.Date/Time/Timestamp, just for test
-        public void testDateWithPattern(@ParamPattern("yyyy-MM-dd HH:mm") @Param("date") java.util.Date since) {
+        public void testDateParamWithPattern(@ParamPattern("yyyy-MM-dd HH:mm") @Param("date") java.util.Date since) {
             System.out.println(since);
         }
 
