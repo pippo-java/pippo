36e8c337b2d2e8c669835d2db837cdefdefb24da
Lorrin Nelson <lorrin@lorrin.org>
2017-07-21 16:53:14 -0700

    Return logic of when to stop to the stop() method

--- pippo-core/src/main/java/ro/pippo/core/Pippo.java
+++ pippo-core/src/main/java/ro/pippo/core/Pippo.java
@@ -163,7 +163,7 @@ public class Pippo implements ResourceRouting, ReloadWatcher.Listener {
 
     public void stop() {
         if (!running) {
-            log.warn("Server is not started");
+            log.debug("Server is not started");
             return;
         }
 
@@ -245,10 +245,7 @@ public class Pippo implements ResourceRouting, ReloadWatcher.Listener {
     }
 
     private void addShutdownHook() {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            if (running)
-                stop();
-        }));
+        Runtime.getRuntime().addShutdownHook(new Thread(Pippo.this::stop));
     }
 
     private Application createApplication() {
