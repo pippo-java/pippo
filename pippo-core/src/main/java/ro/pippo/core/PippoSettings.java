efcd90bc7218b3a942213f1e2cc14753b32b1f23
decebals <decebal.suiu@gmail.com>
2018-10-27 10:48:10 +0300

    Remove unused import

--- pippo-core/src/main/java/ro/pippo/core/PippoSettings.java
+++ pippo-core/src/main/java/ro/pippo/core/PippoSettings.java
@@ -25,7 +25,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
